f373924c2b3304b8fe7a58138bb55b4421d93e49
王立宽 <73371508+AKALucifer@users.noreply.github.com>
2020-11-16 20:57:49 +0800

    Delete text6.py

--- pythonProject1/text6.py
+++
@@ -1,8 +0,0 @@
-#第六题
-for a in range(1,10):
-     for b in range(10):
-         for c in range(10):
-             s1= a*100+b*10+c
-             s2= pow(a,3)+pow(b,3)+pow(c,3)
-             if s1==s2:
-                 print(s1, "是水仙花数")
